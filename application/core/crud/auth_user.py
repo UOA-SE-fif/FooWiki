ea4c117b6170e819b5e6e67f0880ecd8d1c35798
初乎草庭 <1783541507@qq.com>
2023-09-20 20:51:19 +0800

    初步实现login_api和foods_api
    按照自己初步的理解写了一下api

--- application/core/crud/auth_user.py
+++ application/core/crud/auth_user.py
@@ -1,3 +1,4 @@
+from flask import jsonify
 from ...models import *
 import hashlib
 
@@ -21,3 +22,43 @@ def register_user(username, password):
         # Todo: log, status code
         print(e)
         return 1
+
+
+def login_user(username, password):
+    """
+    登录账号
+    :param username: 用户名
+    :param password: 密码
+    :return: 登录成功返回0，否则返回状态码
+    """
+    # SHA256加密
+    password = hashlib.sha256(password.encode("utf-8")).hexdigest()
+    user = UserAuth.query.filter_by(username=username).first()
+    if user:
+        if user.password == password:
+            return 0
+        else:
+            # Todo: log, status code
+            return 1
+    else:
+        # Todo: log, status code
+        return 1
+
+
+def foods_dishes():
+    """
+    :return: 以列表形式返回关于菜品的数据
+    """
+    dishes = DishesBase.query.all()
+    dish_list = []
+    for dish in dishes:
+        dish_data = {
+            'dishid': dish.dishid,
+            'dishname': dish.dishname,
+            'describe': dish.describe,
+            'price': dish.price,
+            'shopname': dish.shopname,
+            'floor': dish.floor
+        }
+        dish_list.append(dish_data)
+    return jsonify(dish_list)
