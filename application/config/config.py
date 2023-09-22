f502a0df6d49cc97c60f58db243db989f4b22520
REllllll <61529225+REllllll@users.noreply.github.com>
2023-09-22 10:44:17 +0800

    Add .idea files (sync pycharm settings)

--- application/config/config.py
+++ application/config/config.py
@@ -1,7 +1,7 @@
 import json
 
 # 打开配置文件
-with open('application\config\config.json', encoding='utf-8') as f:
+with open('application/config/config.json', encoding='utf-8') as f:
     config_dict = json.load(f)
 
 
