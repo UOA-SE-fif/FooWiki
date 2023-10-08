2c38c3727d4e9a466229d013dd6fac66ca1c74ee
REllllll <61529225+REllllll@users.noreply.github.com>
2023-10-08 23:30:56 +0800

    update index.js and create nav_bar.js

--- web/foowiki/components/dropdown_selector.js
+++ web/foowiki/components/dropdown_selector.js
@@ -5,7 +5,7 @@ export default function Selector({name, options, onChange, id}) {
         <div>
             <label htmlFor={name}>{name}</label>
             <select className="form-select" name={name} id={id} onChange={onChange} >
-                <option value="all">全部</option>
+                <option value="0">全部</option>
                 {options.map((option, index) => {
                     return <option key={index} value={option}>{option}</option>
                 })}
