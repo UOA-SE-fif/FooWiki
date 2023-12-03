9a0b1075376cad6e39d9db0b813276fa654b5e76
csf123321 <133735960+csf123321@users.noreply.github.com>
2023-12-03 15:56:40 +0800

    登录功能

--- web/foowiki/pages/_app.tsx
+++ web/foowiki/pages/_app.tsx
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
       <Head>
-        <title>locofy-react-nextjs-project</title>
+        <title>FooWiki</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
--- web/foowiki/pages/login.tsx
+++ web/foowiki/pages/login.tsx
@@ -4,6 +4,7 @@ import useScreenSize from '../Hook/useScreenSize'
 import Container from "../components/container"
 import Form_card from "../components/form_card"
 import Image from "next/image";
+import Link from "next/link";
 import background from "../public/background.png"
 import {username, password} from "../components/form_card";
 
@@ -41,65 +42,6 @@ async function login() {
             alert("登录失败")
         }
     }
-const phoneLogin: NextPage = () => {
-  return (
-    <div className={styles.foowikiLogin}>
-      <div className={styles.navBar}>
-        <img className={styles.listIcon} alt="" src="/list.svg" />
-        <div className={styles.foowiki}>FooWiki</div>
-      </div>
-      <div className={styles.login}>
-        <div className={styles.title}>
-          <div className={styles.title1}>Login</div>
-        </div>
-        <div className={styles.loginCard}>
-          <div className={styles.container} />
-          <div className={styles.title2}>
-            <div className={styles.title1}>Email/Username</div>
-          </div>
-          <div className={styles.selector}>
-            <div className={styles.container1} />
-            <div className={styles.stFloor}>1st Floor</div>
-            <img className={styles.selectorChild} alt="" src="/group-2.svg" />
-          </div>
-          <div className={styles.title4}>
-            <div className={styles.title1}>Password</div>
-          </div>
-          <div className={styles.selector1}>
-            <div className={styles.container1} />
-            <div className={styles.stFloor}>1st Floor</div>
-            <img className={styles.selectorChild} alt="" src="/group-2.svg" />
-          </div>
-        </div>
-      </div>
-      <div className={styles.noAccountCreateContainer}>
-        <span>{`No account?   `}</span>
-        <span className={styles.createOne}>Create one</span>
-      </div>
-      <div className={styles.button}>
-        <div className={styles.container1} />
-        <div className={styles.login1}>Login</div>
-      </div>
-    </div>
-  );
-};
-
-const pcLogin: NextPage = () =>{
-    return (
-        <Container>
-            <Form_card cardType={"login"} func={login}>
-            </Form_card>
-            <Image src={background} alt="background"
-                   layout="fill"
-                   objectFit="cover"
-                   quality={100}
-                   priority={true}
-                   style={{zIndex: -1}}
-            ></Image>
-        </Container>
-    )
-}
-
 const Login: NextPage = () =>{
     const screenSize = useScreenSize();
     if(screenSize.width<changeWidth)
@@ -119,7 +61,7 @@ const Login: NextPage = () =>{
             <div className={styles.title1}>Email/Username</div>
           </div>
           <div className={styles.selector}>
-            <div className={styles.container1} />
+            <input ref={username} className={styles.container1} />
             <div className={styles.stFloor}>1st Floor</div>
             <img className={styles.selectorChild} alt="" src="/group-2.svg" />
           </div>
@@ -127,7 +69,7 @@ const Login: NextPage = () =>{
             <div className={styles.title1}>Password</div>
           </div>
           <div className={styles.selector1}>
-            <div className={styles.container1} />
+            <input ref={password} type="password" className={styles.container1} />
             <div className={styles.stFloor}>1st Floor</div>
             <img className={styles.selectorChild} alt="" src="/group-2.svg" />
           </div>
@@ -135,11 +77,12 @@ const Login: NextPage = () =>{
       </div>
       <div className={styles.noAccountCreateContainer}>
         <span>{`No account?   `}</span>
-        <span className={styles.createOne}>Create one</span>
+        <Link href="/register" className={styles.createOne}>Create one</Link>
       </div>
-      <div className={styles.button}>
-        <div className={styles.container1} />
+      <div className={styles.button} >
+        <button className={styles.container1} onClick={login}>
         <div className={styles.login1}>Login</div>
+        </button>
       </div>
     </div>
   );
